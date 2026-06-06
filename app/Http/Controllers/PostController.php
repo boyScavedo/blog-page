58783d8d77c002a27be0c51b332fdd427c995bd7
boyScavedo <jeevanadhikaritech@gmail.com>
2026-06-06 19:53:07 +0545

    initial commit + basic blade dataflow testing

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorepostRequest;
+use App\Http\Requests\UpdatepostRequest;
+use App\Models\post;
+use Illuminate\Support\Str;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $markdown = "# Hello World";
+        $content = Str::markdown($markdown);
+
+
+        return view('page.home', compact('content'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorepostRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatepostRequest $request, post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(post $post)
+    {
+        //
+    }
+}
